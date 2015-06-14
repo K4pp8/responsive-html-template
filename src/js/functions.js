f7bd5e207addc51bb84c697c107b274fd007100d
K4pp8 <k.bojaxhiu@gmail.com>
2015-06-14 14:26:37 +0200

    Aggiunta lista a scomparsa su work

--- src/js/functions.js
+++ src/js/functions.js
@@ -2,9 +2,27 @@ $( document ).ready(function() {
 
   smoothScroll(300);
 
+  workBelt();
+
 });
 
-function smoothScroll (duration) {
+function workBelt() {
+	$('.thumb-unit').on( 'click' , function(){
+
+		$('.work-belt').css('left' , '-100%');
+		$('.work-container').show();
+
+	});
+
+	$('.work-return').on( 'click' , function(){
+
+		$('.work-belt').css('left' , '0%');
+		$('.work-container').hide(800);
+
+	});
+}
+
+function smoothScroll(duration) {
 	$('a[href^="#"').on( 'click' ,function(event) {
 
 		var target = $( $(this).attr('href') );
