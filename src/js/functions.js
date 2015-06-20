044e9e7bd89cfdb056150332fc598d4cc3717eb1
K4pp8 <k.bojaxhiu@gmail.com>
2015-06-20 21:24:29 +0200

    Fix menu in modalita mobile e aggiunto fittext js

--- src/js/functions.js
+++ src/js/functions.js
@@ -8,6 +8,8 @@ $( document ).ready(function() {
 
   clientload();
 
+  jQuery("header h1").fitText(1.2, { minFontSize: '20px', maxFontSize: '92px' });
+
 });
 
 function clientload() {
@@ -91,3 +93,35 @@ function smoothScroll(duration) {
 
 	});
 }
+(function( $ ){
+
+  $.fn.fitText = function( kompressor, options ) {
+
+    // Setup options
+    var compressor = kompressor || 1,
+        settings = $.extend({
+          'minFontSize' : Number.NEGATIVE_INFINITY,
+          'maxFontSize' : Number.POSITIVE_INFINITY
+        }, options);
+
+    return this.each(function(){
+
+      // Store the object
+      var $this = $(this);
+
+      // Resizer() resizes items based on the object width divided by the compressor * 10
+      var resizer = function () {
+        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
+      };
+
+      // Call once to set.
+      resizer();
+
+      // Call on resize. Opera debounces their resize by default.
+      $(window).on('resize.fittext orientationchange.fittext', resizer);
+
+    });
+
+  };
+
+})( jQuery );
